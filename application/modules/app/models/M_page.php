5733c452ba818de2d8d81c66b04c365d8c4d27d5
yfeislan07 <yusufeislan123@gmail.com>
2025-01-08 05:56:22 +0700

    get data perangkat desa

---
+++ application/modules/app/models/M_page.php
@@ -0,0 +1,21 @@
+<?php
+
+Class M_page extends CI_Model {
+
+    public function get_identitas()
+    {
+        $sql = "SELECT * FROM master_identitas";
+        $query = DB::raw('row_array', $sql);
+        return $query;
+    }
+
+    public function get_perangkat_desa()
+    {
+        $sql = "SELECT a.*, b.jabatan_nm 
+                FROM data_perangkat_desa a
+                LEFT JOIN master_jabatan b ON a.jabatan_id = b.jabatan_id
+                LIMIT 4";
+        $query = DB::raw('result_array', $sql);
+        return $query;
+    }
+}
