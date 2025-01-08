f7baadff22b05ccd5eb982eeccaf9986dff9b07b
yfeislan07 <yusufeislan123@gmail.com>
2025-01-08 09:00:19 +0700

    update auto load

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'itm', 'db', 'dblog', 'cookie', 'text', 'notifikasi', 'kyc');
+$autoload['helper'] = array('url', 'itm', 'db');
 
 /*
 | -------------------------------------------------------------------
